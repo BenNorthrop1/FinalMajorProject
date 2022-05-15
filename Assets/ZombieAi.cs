019609875000e2e669692f5c393b8185dc504129
BenNorthrop1 <90899118+BenNorthrop1@users.noreply.github.com>
2022-05-15 20:41:40 +0100

    update

--- Assets/ZombieAi.cs
+++ Assets/ZombieAi.cs
@@ -7,12 +7,25 @@ public class ZombieAi : MonoBehaviour
 {
 
 NavMeshAgent nm;
-public Transform target;
 
-void Awake()
+private Transform target;
+
+public float distanceThreshold = 10f;
+
+public enum AiState{ idle, chasing};
+
+public AiState aiState = AiState.idle;
+
+private Animator animator;
+
+
+
+void Start()
 {
+    target = GameObject.FindGameObjectWithTag("Player").transform;
+    animator = GetComponent<Animator>();
     nm = GetComponent<NavMeshAgent>();
-
+    StartCoroutine(Think());
 }
 
 void Update()
@@ -25,9 +38,38 @@ IEnumerator Think()
 {
     while(true)
     {
-        nm.SetDestination(target.position);
+        switch (aiState)
+        {
+            case AiState.idle:
+            
+            float dist = Vector3.Distance(target.position, transform.position);
 
-        yield return new WaitForSeconds(1f);
+            if(dist < distanceThreshold)
+            {
+                aiState = AiState.chasing;
+                animator.SetBool("Chasing", true);
+            }
+
+            nm.SetDestination(transform.position);
+
+                break;
+            case AiState.chasing:
+
+                dist = Vector3.Distance(target.position, transform.position);
+
+                if(dist > distanceThreshold)
+                {
+                    aiState = AiState.idle;
+                    animator.SetBool("Chasing", false);
+                }
+
+                nm.SetDestination(target.position);
+
+                break;
+            default:
+                break;
+        }
+        yield return new WaitForSeconds(0.2f);
 
     }
 }
